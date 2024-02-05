780c2376773094b621992cd86a0f1a408cb4506b
irawancandra6699 <134005006+irawancandra6699@users.noreply.github.com>
2024-02-05 18:01:26 +0700

    Update bot1.sh

--- bot1.sh
+++ bot1.sh
@@ -7,7 +7,7 @@ rm -rf cybervpn
 apt update && apt upgrade -y
 apt install python3 python3-pip -y
 apt install sqlite3 -y
-wget bot.kvm-xcode.biz.id/bot/cybervpn.zip
+wget kvm-xcode.biz.id/azg/cybervpn.zip
 unzip cybervpn.zip
 cd cybervpn
 rm var.txt
